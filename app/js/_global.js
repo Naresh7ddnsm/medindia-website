2ce5144cd80363a23d48141d4770af985ce69066
naresh7ddnsm <naresh.dn777@gmail.com>
2019-11-11 22:41:14 +0530

    Home page development

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,14 +1,24 @@
 // Global
 var $d = $(document),
     $w = $(window),
+    $body = $('body'),
     hamburgerMenu = $('#canvase'),
-    hamburgerMenuTrigger = $('[hamburget__trigger]')
+    hamburgerMenuTrigger = $('[hamburget__trigger]'),
+    m_gsearch = $('#gsearch__container'),
+    m_gsearchTrigger = $('[gsearch-trigger]'),
+    m_gsearchcloud__bg = $('#gsearchcloud__bg'),
+    trigger_tab = $('.tab-item');
+    
 app.global = {
     device: function(){
         return (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
     },
     events: function(){
         hamburgerMenuTrigger.on('click', this.hamburgerMenu.bind(this));
+        m_gsearchTrigger.on('click', this.gsearch.bind(this));
+        m_gsearchcloud__bg.on('click', this.gsearch_close.bind(this));
+        //m_gsearchTrigger.on('mouseout', this.gsearch.bind(this, 'close'));
+        trigger_tab.on('click', this.handler_tab.bind(this));
     },
     init: function(){ 
         //this.device() ? null : this.slickIcon();
@@ -53,7 +63,52 @@ app.global = {
         var ele = $(e.target), 
             STATUS = ele.data('status');
         STATUS == 'open' ? hamburgerMenu.addClass('open') : hamburgerMenu.removeClass('open')
+        STATUS == 'open' ? this.bodyScrollBlock(true) : this.bodyScrollBlock(!true)
+
+    },
+    bodyScrollBlock: function(STATUS){
+        STATUS ? $body.addClass('scroll-block') : $body.removeClass('scroll-block');
+    },
+    gsearch_open: function(){
+        m_gsearch.slideDown()
+        this.bodyScrollBlock(true)
+    },
+    gsearch_close: function(){
+        m_gsearch.slideUp();
+        this.bodyScrollBlock(!true)
+    },
+    gsearch: function(e){
+        _this = this;
+        m_gsearch.slideToggle(360, function(){
+            m_gsearch.css('display') == 'block' ? _this.bodyScrollBlock(true) : _this.bodyScrollBlock(!true)
+        });
+        /*var ele = $(e.target), 
+            STATUS = ele.data('status');
+        if(typeof STATUS === typeof undefind){
+            ele.attr('data-status', 'open');
+        }
+        STATUS = ele.attr('data-status');
+        if(STATUS == 'open'){
+            this.gsearch_open()
+            ele.attr('data-status', 'close');
+            this.bodyScrollBlock(true)
+        } else {
+            this.gsearch_close();
+            ele.attr('data-status', 'open');
+            this.bodyScrollBlock(!true)
+        }*/
+    },
+    handler_tab: function(e){
+        var ele =  $(e.target),
+            target = ele.data('target'),
+            parent = ele.closest('.tab__container'),
+            content = parent.find('.tab-content');
+        ele.find('.tab-link').addClass('active')
+        ele.siblings().find('.tab-link').removeClass('active')
+        parent.find('.tab-content').hide();
+        parent.find(target).show();
     }
+
 }
 
 // Run the global stuff
